1cc1672546246695e10d2132aed3a27da03fb1e6
tuomax7 <tuomas.nummela.per@gmail.com>
2025-05-17 22:21:53 +0200

    feat: add schadcn/ui and an example landing ui view

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,14 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   server: {
     host: "0.0.0.0",
     port: 3000,
     proxy: {
       "/api": "http://localhost:8080",
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
